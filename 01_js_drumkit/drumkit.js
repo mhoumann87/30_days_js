cd72018b8f1bde9012e3c0d3aca48e2f5f52cbc7
Michael Houmann <michael@michael-h.dk>
2018-09-19 12:18:49 +0200

    Add frontpage for the project

--- 01_js_drumkit/drumkit.js
+++ 01_js_drumkit/drumkit.js
@@ -1,7 +1,9 @@
-(function () {
-  const pos = document.getElementById("copy");
-  let year = new Date().getFullYear();
-  (year === 2018) ?
-    pos.innerHTML = `<i class="far fa-copyright"></i>${year}` :
-    pos.innerHTML = `<i class="far fa-copyright"></i>2018 - ${year}`;
+// Set the year for the copyright message in the footer on start up
+(function() {
+    const pos = document.getElementById("copy");
+    let year = new Date().getFullYear();
+    (year === 2018) ?
+        pos.innerHTML = `<i class="far fa-copyright"></i>${year}` :
+        pos.innerHTML = `<i class="far fa-copyright"></i>2018 - ${year}`;
 })();
+
