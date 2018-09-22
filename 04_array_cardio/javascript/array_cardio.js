31bbdc69c4dfd0c517a2fdc6a0d87000526602c5
Michael Houmann <michael@michael-h.dk>
2018-09-22 17:23:14 +0200

    Add the filter exercise with js, css and html. Frontpage updated

--- 04_array_cardio/javascript/array_cardio.js
+++ 04_array_cardio/javascript/array_cardio.js
@@ -7,6 +7,8 @@
     pos.innerHTML = `<i class="far fa-copyright"></i>2018 - ${year}`
 })();
 
+// Data sets used for the page
+
 const inventors = [
   { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
   { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
@@ -24,3 +26,90 @@ const inventors = [
 
 const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
+// Inventors section
+
+// Show all data in a table on the page
+const dataSetPos = document.getElementById('invData');
+
+function showData() {
+  let invHtml = `<table>
+  <tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Born</th>
+    <th>Passed</th>
+  </tr>`;
+  inventors.forEach(inventor => {
+    invHtml += `<tr><td>${inventor.first}</td><td>${inventor.last}</td><td>${inventor.year}</td><td>${inventor.passed}</td></tr>`;
+  });
+
+  invHtml += `</table>`;
+  dataSetPos.innerHTML = invHtml;
+};
+showData();
+
+//the filter() method
+
+const invError = document.querySelector('.inv-errror');
+const show = document.querySelector('.invGrid');
+
+// Run the function when the form is submitted
+document.querySelector('#show-inv').addEventListener('submit', showInventors);
+
+function showInventors(event) {
+  //clear the inventor div, if there are any shown there
+  show.innerHTML = '';
+  //prevent the page from refreshing on submit
+  event.preventDefault();
+
+  //get the values the user entered
+  let invLow = document.querySelector('#invLow').value;
+  let invHigh = document.querySelector('#invHigh').value;
+
+  // clean up in the input
+  if (invLow === '' || invHigh === '') {
+    // Show error message in either field are empty;
+    invError.innerText = 'You neeed to fill both fields';
+  } else if (invLow === invHigh) {
+    //Show error message if it is the same numbers in both fields
+    invError.innerText = 'You need to give two different numbers';
+  } else {
+
+    // if the user entered the opposit info switch the numbers
+    let yearOne, yearTwo;
+
+    if (invLow > invHigh) {
+      yearOne = invHigh;
+      yearTwo = invLow;
+    } else {
+      yearOne = invLow;
+      yearTwo = invHigh;
+    }
+
+    // Time to do the filtering
+    const filteredInventors = inventors.filter(inventor => (inventor.year >= yearOne && inventor.year < yearTwo));
+
+    if (filteredInventors.length < 1) {
+      document.querySelector('#show-inv').reset();
+      invError.innerText = '';
+      invError.innerText = `There are no inventors born between ${yearOne} and  ${yearTwo}`;
+    } else {
+      document.querySelector('#show-inv').reset();
+      invError.innerText = '';
+      let html = ``;
+      filteredInventors.forEach(inventor => {
+        html += `
+        <div class="inventor">
+          <img src="./images/${inventor.last.toLocaleLowerCase()}.jpg" alt="${inventor.first} ${inventor.last}" />
+          <h5>${inventor.first} ${inventor.last}</h5>
+          <p>Born ${inventor.year}, passed ${inventor.passed}</p>
+          <small>Image from <a href="https://www.wikipedia.org/" target="_blank">Wikipedia</a></small>
+        </div>
+      `;
+      });
+      show.innerHTML = html;
+    }
+  }
+
+
+}
