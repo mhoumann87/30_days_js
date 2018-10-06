4db683a54f4995f74d613953fe1419608b9f0d94
Michael Houmann <michael@michael-h.dk>
2018-10-06 11:05:56 +0200

    Add project 14 HTML and CSS

---
+++ 14_javascript_references_vs_copying/javascript/object_arrays.js
@@ -0,0 +1,4 @@
+//Copyright
+const pos = document.getElementById('copy');
+const year = new Date().getFullYear();
+(year === 2018) ? pos.innerHTML = `<i class="far fa-copyright"></i>2018`: pos.innerHTML = `<i class="far fa-copyright"></i>2018 - ${year}`;
