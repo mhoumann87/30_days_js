4d55dd6f0af9ca7fc211c8d29339407a6b93b441
Michael Houmann <michael@michael-h.dk>
2018-10-10 07:51:31 +0200

    Add project 17 base setup with html and css

---
+++ 17_sorting_band_names/javascript/sort_band_names.js
@@ -0,0 +1,4 @@
+// Copyright info
+const pos = document.querySelector('#copy');
+const year = new Date().getFullYear();
+(year === 2018) ? pos.innerHTML = `<i class="far fa-copyright"></i>2018`: pos.innerHTML = `<i class="far fa-copyright"></i>2018 - ${year}`;
