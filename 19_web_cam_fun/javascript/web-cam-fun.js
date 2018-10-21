5dba0de80db4512b1078477dca93b03673234f67
Michael Houmann <michael@michael-h.dk>
2018-10-21 14:36:16 +0200

    Add javascript for webcam fun, index.html is updated

--- 19_web_cam_fun/javascript/web-cam-fun.js
+++ 19_web_cam_fun/javascript/web-cam-fun.js
@@ -1,25 +1,49 @@
 //copyright
-const pos = document.querySelector("#copy");
+const pos = document.querySelector('#copy');
 const year = new Date().getFullYear();
-year === 2018
-  ? (pos.innerHTML = `<i class="far fa-copyright"></i>2018`)
-  : (pos.innerHTML = `<i class="far fa-copyright"></i>2018 - ${year}`);
+year === 2018 ?
+  (pos.innerHTML = `<i class="far fa-copyright"></i>2018`) :
+  (pos.innerHTML = `<i class="far fa-copyright"></i>2018 - ${year}`);
 
 /* 
  * Web cam fun
  */
 
-// Get the selectors for the webcam
-const video = document.querySelector(".player");
-const canvas = document.querySelector(".photo");
-const ctx = canvas.getContext("2d");
-const strip = document.querySelector(".strip");
-let pixels;
+//Get all the elements on the page
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const strip = document.querySelector('.strip');
+const choose = document.querySelector('form');
+const controls = document.querySelector('.controls');
 
-// Get the video feed from the webcam
+
+const ctx = canvas.getContext('2d');
+const levels = {};
+let effect = 'photoBooth';
+
+choose.addEventListener('change', setEffect);
+
+function setEffect(event) {
+  effect = event.target.value;
+  setControls();
+  paintToCanvas();
+}
+
+function setControls() {
+  if (effect === 'redEffect') {
+    controls.innerHTML = `<h4>Red Effect</h4> `;
+  } else if (effect === 'rgb') {
+    controls.innerHTML = `<h4>RGB Effect</h4> `;
+  } else {
+    controls.innerHTML = `<button class="takePhoto">Take Photo</button>`;
+  }
+}
+
 function getVideo() {
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
+  navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false
+    })
     .then(localMediaStream => {
       video.srcObject = localMediaStream;
       video.play();
@@ -28,92 +52,65 @@ function getVideo() {
       console.error(err);
     });
 }
-function paintVideoToCanvas(pixels) {
+
+function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
   canvas.width = width;
   canvas.height = height;
 
+  if (effect === 'photoBooth') {
+    takePhoto();
+  }
+
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-    pixels = ctx.getImageData(0, 0, width, height);
-    //pixels.rgbSplit(pixels);
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    if (effect === 'redEffect') {
+      pixels = redEffect(pixels);
+    }
+
+    if (effect === 'rgb') {
+      pixels = rgb(pixels);
+    }
+
     ctx.putImageData(pixels, 0, 0);
-  }, 16);
-}
-// Get the checkboxes for selecting functtions
-document.querySelector("form").addEventListener("change", chooseEffect);
-const controls = document.querySelector(".controls");
 
-function chooseEffect(event) {
-  switch (event.target.value) {
-    case "photoBooth":
-      photoBooth();
-      break;
-    case "greenScreen":
-      greenScreen();
-      break;
-    case 'redEffect':
-      redEffect();
-      break;
-    case "rgb":
-      rgb();
-      break;
-  }
+  });
 }
 
-function photoBooth() {
-  controls.innerHTML = `<button class="takePhoto">Take Photo</button>`;
+function takePhoto() {
   document.querySelector('.takePhoto').addEventListener('click', function () {
 
     const data = canvas.toDataURL('image/jpeg');
     const link = document.createElement('a');
     link.href = data;
-    link.setAttribute('download', 'picture');
-    link.innerHTML = `<img src="${data}" alt="great picture">`;
+    link.setAttribute('download', 'image');
+    link.innerHTML = `<img src="${data}" alt="Image from Photo Booth">`;
     strip.insertBefore(link, strip.firstChild);
   });
-
-  getVideo();
 }
 
-function greenScreen() {
-  controls.innerHTML = ``;
-}
-
-function rgb() {
-  controls.innerHTML = `
-      <div class="red slider">
-        <label for="rmin">Red Min:</label>
-        <input type="range" min=0 max=255 name="rmin">
-        <label for="rmax">Red Max:</label>
-        <input type="range" min=0 max=255 name="rmax">
-      </div>
-      <div class="green slider">
-        <label for="gmin">Green Min:</label>
-        <input type="range" min=0 max=255 name="gmin">
-        <label for="gmax">Green Max:</label>
-        <input type="range" min=0 max=255 name="gmax">
-        </div>
-      <div class="blue slider">
-        <label for="bmin">Blue Min:</label>
-        <input type="range" min=0 max=255 name="bmin">
-        <label for="bmax">Blue Max:</label>
-        <input type="range" min=0 max=255 name="bmax">
-      </div>
-  `;
-}
-
-function redEffect() {
-  controls.innerHTML = ``;
+function redEffect(pixels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
     pixels.data[i + 0] = pixels.data[i + 0] + 200;
     pixels.data[i + 1] = pixels.data[i + 1] - 50;
     pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
   }
-  drawImage(pixels);
-  getVideo();
+  return pixels;
 }
 
-photoBooth();
-video.addEventListener('canplay', paintVideoToCanvas);
+function rgb(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0];
+    pixels.data[i + 500] = pixels.data[i + 1];
+    pixels.data[i - 550] = pixels.data[i + 2];
+  }
+  return pixels;
+}
+
+
+setControls();
+getVideo();
+video.addEventListener('canplay', paintToCanvas);
