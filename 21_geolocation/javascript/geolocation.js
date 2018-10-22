ffa009ad4a44a90ec6f671ac0089d6823de0dc6c
Michael Houmann <michael@michael-h.dk>
2018-10-22 13:45:48 +0200

    Add geolocation function

--- 21_geolocation/javascript/geolocation.js
+++ 21_geolocation/javascript/geolocation.js
@@ -1,1 +1,17 @@
-console.log(config.api_key);
+//console.log(config.api_key);
+const msg = document.querySelector('.message');
+
+function getGeolocation() {
+  msg.innerHTML = `Your browser do not support geolocation`;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getLocation)
+  } else {
+    msg.innerHTML = `Your browser do not support geolocation`;
+  }
+}
+
+function getLocation(position) {
+  console.log(position);
+}
+
+getGeolocation();
