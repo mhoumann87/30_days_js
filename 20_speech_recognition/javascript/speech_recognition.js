8281b75369e1ec339664a4205edd055840203032
Michael Houmann <michael@michael-h.dk>
2018-10-22 08:58:09 +0200

    Add javascript, html and css  for speech recognition project

--- 20_speech_recognition/javascript/speech_recognition.js
+++ 20_speech_recognition/javascript/speech_recognition.js
@@ -7,3 +7,32 @@ const year = new Date().getFullYear();
 /* 
  * Speech Recognition 
  */
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+recognition.lang = 'en-US';
+
+let p = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+recognition.addEventListener('result', event => {
+  //console.log(event.results);
+  const transcript = Array.from(event.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+
+  p.textContent = transcript;
+
+  if (event.results[0].isFinal) {
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
+
+});
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
